f970f8ac63f6e753b00b64febadd46aaae886523
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-12-18 17:02:59 +0100

    test: 💍 Remove unused snapshot tests

--- packages/example/components/exampleComponents/Text/__tests__/Text.example.test.tsx
+++ packages/example/components/exampleComponents/Text/__tests__/Text.example.test.tsx
@@ -4,11 +4,6 @@ import TextExample from '../Text.example';
 import { renderComponentToWebScreenshot } from 'kin';
 
 describe('TextExample', () => {
-  it('should render the TextExample component correctly', () => {
-    const { toJSON } = render(<TextExample />);
-    expect(toJSON()).toMatchSnapshot();
-  });
-
   it('should match image snapshot', async () => {
     const renderedComponent = render(<TextExample />);
 
