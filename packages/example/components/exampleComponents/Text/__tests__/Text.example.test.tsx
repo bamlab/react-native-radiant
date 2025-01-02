3a2525726e831b26b2cfa811642afeddd8e49412
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-01-02 15:42:36 +0100

    fix: 🐛 Update RN radiant function names used in test

--- packages/example/components/exampleComponents/Text/__tests__/Text.example.test.tsx
+++ packages/example/components/exampleComponents/Text/__tests__/Text.example.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import TextExample from '../Text.example';
-import { renderComponentToWebScreenshot } from 'react-native-radiant';
+import { getVisualSnapshot } from 'react-native-radiant';
 
 describe('TextExample', () => {
   it('should match image snapshot', async () => {
     const renderedComponent = render(<TextExample />);
 
-    expect(await renderComponentToWebScreenshot({ renderedComponent })).toMatchImageSnapshot();
+    expect(await getVisualSnapshot(renderedComponent)).toMatchImageSnapshot();
   });
 });
