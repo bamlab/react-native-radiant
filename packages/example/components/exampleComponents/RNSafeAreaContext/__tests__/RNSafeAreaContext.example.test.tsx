a78c2191c7d9c5c79ed923f56395017fc9d771e5
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-01-02 16:50:25 +0100

    refactor: 💡 Use workspace protocol to import radiant package

--- packages/example/components/exampleComponents/RNSafeAreaContext/__tests__/RNSafeAreaContext.example.test.tsx
+++ packages/example/components/exampleComponents/RNSafeAreaContext/__tests__/RNSafeAreaContext.example.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import RNSafeAreaContextExample from '../RNSafeAreaContext.example';
-import { getVisualSnapshot } from 'react-native-radiant';
+import { getVisualSnapshot } from '@bam.tech/react-native-radiant';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 jest.mock(
