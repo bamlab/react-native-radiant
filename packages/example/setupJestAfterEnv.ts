e50541928828374dd79d12758a6b48477c29e31d
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-01-07 17:30:43 +0100

    feat: 🎸 Add tolerance option for snapshot testing

--- packages/example/setupJestAfterEnv.ts
+++ packages/example/setupJestAfterEnv.ts
@@ -1,8 +1,19 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 
-const toMatchImageSnapshot = configureToMatchImageSnapshot({
-  failureThreshold: 16,
-  failureThresholdType: 'pixel',
-});
+const snapshotToleranceLevel = process.env.SNAPSHOT_TOLERANCE_LEVEL == 'HIGH' ? 'HIGH' : 'LOW'; // default to low tolerance level
+
+const isSnapshotToleranceLow = snapshotToleranceLevel === 'LOW';
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot(
+  isSnapshotToleranceLow
+    ? {
+        failureThreshold: 16,
+        failureThresholdType: 'pixel',
+      }
+    : {
+        failureThreshold: 0.01,
+        failureThresholdType: 'percent',
+      },
+);
 
 expect.extend({ toMatchImageSnapshot });
